760f8df5c2593dce665f9080078471ec146b6b71
JhonCamargo07 <jhonalexcamargo07@gmail.com>
2023-07-30 15:35:58 -0500

    Se realiza ajustes al proyecto, se crea el archivo base para las configuraciones del programa

--- main.py
+++ main.py
@@ -1,27 +1,57 @@
 import os
+import sys
 import time
+import datetime
 
-def watch_folder(folder_path):
-    # Obtener los archivos actuales en la carpeta
-    current_files = set(os.listdir(folder_path))
+
+def get_last_modified_date(folder_path):
+    last_modified_date_by_file = {}
+
+    for file in os.listdir(folder_path):
+        try:
+            # Obtener la información del archivo
+            file_stat = os.stat(os.path.join(folder_path, file))
+
+            # Obtener el timestamp de la última modificación del archivo
+            last_modified_timestamp = file_stat.st_mtime
+
+            # Convertir el timestamp a una fecha legible
+            last_modified_date = datetime.datetime.fromtimestamp(last_modified_timestamp)
+
+            last_modified_date_by_file.setdefault(file, last_modified_date)
+
+        except FileNotFoundError:
+
+            print(f"El archivo '{folder_path}' no existe.")
+            return None
+
+        except Exception as e:
+
+            print(f"Error al obtener la fecha de modificación: {str(e)}")
+            return None
+
+    return last_modified_date_by_file
+
+
+try:
+    print('Watching for changes. Press Ctrl + C to stop.')
 
     while True:
-        # Obtener los archivos actuales nuevamente
-        updated_files = set(os.listdir(folder_path))
+        folder_path = sys.argv[1] if len(sys.argv) > 1 else os.getcwd()
 
-        # Comparar si hay cambios en los archivos
-        diff = updated_files - current_files
+        # Obtener la última fecha de modificación
+        first_last_modified_date_files = get_last_modified_date(folder_path)
+        time.sleep(5)
+        second_last_modified_date_file = get_last_modified_date(folder_path)
 
-        if diff:
-            print("La carpeta {} ha sido modificada.".format(folder_path))
-            # Coloca aquí las acciones que deseas realizar cuando detecte un cambio en la carpeta.
-            print("Ejecutando comando")
+        for (key_first, value_first), (key_second, value_second) \
+                in zip(first_last_modified_date_files.items(), second_last_modified_date_file.items()):
+            if key_first != key_second:
+                print(f'Cambió el archivo {key_first}')
+                break
+            if value_first != value_second:
+                print(f'Cambió el archivo {key_first}')
+                break
 
-        current_files = updated_files
-        time.sleep(1)
-
-
-if __name__ == "__main__":
-    import sys
-    folder_path = sys.argv[1] if len(sys.argv) > 1 else os.getcwd()
-    watch_folder(folder_path)
+except KeyboardInterrupt as e:
+    print("Gracias por utilizar este programa")
